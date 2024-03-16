7e32128ff88eb1d53e9538cc9088ad0cf9fe8634
Aintme187 <128763833+Aintme187@users.noreply.github.com>
2024-03-16 15:31:05 +0800

    change the csrftoken

--- frontend/src/views/global.vue
+++ frontend/src/views/global.vue
@@ -5,12 +5,12 @@ axios.defaults.withCredentials = true
 
 function get_csrf_token() {
   axios.get("http://localhost:8000/get_csrf_token/")
-  let cookie = document.cookie
-  //console.log('cookie_csrf_token=' + csrf_token)
-  return cookie.split('=')[1]
+  let match = document.cookie.match('(^| )csrftoken=([^;]+)')
+  console.log('cookie_csrf_token=' + match[2])
+  return match[2]
 }
 
-export {axios,get_csrf_token}
+export {axios, get_csrf_token}
 </script>
 
 <template>
