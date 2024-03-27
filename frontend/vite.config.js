dc8d38ffbb557a851c7420ef9a06ae83850a5bdf
Aintme187 <128763833+Aintme187@users.noreply.github.com>
2024-03-27 11:27:48 +0800

    change dockerfile and urls in code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // icon
 
@@ -13,42 +13,44 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    , AutoImport({
-      resolvers: [ // 自动导入图标组件
-    
-        IconsResolver({
-          prefix: 'Icon',
-        }),
-        ElementPlusResolver()
-      ],
-    }),
-    Components({
-      resolvers: [ // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ['ep'],
-        })
-        , ElementPlusResolver()
-      ],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ // 自动导入图标组件
 
-    }),
-    Icons({
-      autoInstall: true,
-    }),
-  ],
-  server: {
-    host: '0.0.0.0', // ip
-    port: 5173,  
-    hmr: true,  // 热启动
-    open: true // 自动打开浏览器
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+                IconsResolver({
+                    prefix: 'Icon',
+                }),
+                ElementPlusResolver()
+            ],
+        }),
+        Components({
+            resolvers: [ // 自动注册图标组件
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                })
+                , ElementPlusResolver()
+            ],
+
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+    ],
+    server: {
+        host: '0.0.0.0', // ip
+        port: 5173,
+        hmr: true,  // 热启动
+        open: true // 自动打开浏览器
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        chunkSizeWarningLimit: 1500,
     }
-  }
 })
